b6ea0300701a1852af56ef4b134f4fa35ea63868
chengxiao19961022 <769478099@qq.com>
2018-12-24 10:53:52 +0800

    封装

--- buildVocab.py
+++ buildVocab.py
@@ -1,17 +1,50 @@
+'''建立词汇表
+
+每一个特征分别建立对应的词汇表
+
+'''
 from gensim.models import Word2Vec
 
-def addTrain(model, sentence):
+
+def add_train(model, sentence):
+    """继续添加词汇
+
+    继续添加词汇，调用model.wv.vocab得到词汇表
+
+    :param model: word2vec model
+    :param sentence: add sentence
+    :return: new model
+    """
+
     model.build_vocab(sentence, update=True)
     model.train(sentence, total_examples=model.corpus_count, epochs=model.iter)
+    return model
 
-sentences = [['first', 'sentence'], ['second', 'sentence']]
-model = Word2Vec(sentences, min_count=1)
+def build_vocab(sentences):
+    '''建立词汇表
 
-print(model.wv.vocab['first'].index)
-# modelpath = 'test.model'
-# model = Word2Vec.load('test.model')
-# model.save(modelpath)
+    建立词汇表，调用model.wv.vocab得到词汇表
 
-addtr = [["hello", "world"]]
-addTrain(model, addtr)
-print(model.wv.vocab)
+    :param sentences: 待建立词汇表的所有词汇
+    :return: model
+    '''
+
+    model = Word2Vec(sentences, min_count=1)
+    return model
+
+def main():
+    # 待建词汇表
+    sentences = [['first', 'sentence'], ['second', 'sentence']]
+    model = build_vocab(sentences)
+    print(model.wv.vocab['first'].index)
+    # modelpath = 'test.model'
+    # model = Word2Vec.load('test.model')
+    # model.save(modelpath)
+
+    addtr = [["hello", "world"]]
+    model = add_train(model, addtr)
+    print(model.wv.vocab)
+
+
+if __name__ == '__main__':
+    main()
