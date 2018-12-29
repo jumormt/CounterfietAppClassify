4d9ba0f77fd156d46554d2e00ef444558e862a4d
chengxiao19961022 <769478099@qq.com>
2018-12-29 18:10:38 +0800

    add jsonsim

--- main.py
+++ main.py
@@ -1,9 +1,73 @@
+'''
+main
+'''
+__author__ = 'chengxiao'
 from text_analysis import build_vocab_all as bva, calc_textsim as cts
 import os
 from common import calc_sim as cs
-__author__ = 'chengxiao'
+import json
+from common import build_vocab as bv
 
-def process(long_des_dir, short_des_dir):
+
+def get_json_sim(json_dirpath):
+    '''
+
+    :param json_dirpath:
+    :return: key为pair对，vaule为以各个比较项为键值的相似度
+    '''
+    files = {}
+    for dirpath, dirnames, filenames in os.walk(json_dirpath):
+        for file in filenames:
+            fullpath = os.path.join(dirpath, file)
+            with open(fullpath, 'r', encoding='utf-8') as f:
+                fs = json.load(f)
+                files[file] = {'json': fs, 'filepath': fullpath}
+    models = dict()
+
+    sentences = []
+    for key in files:
+        fj = files[key]['json']['details']['app_details']['app_category']
+        sentences.append(fj)
+    models['app_category'] = bv.build_vocab(sentences)
+    sentences = []
+
+    sentences = []
+    for key in files:
+        fj = files[key]['json']['details']['app_details']['app_type']
+        sentences.append([fj])
+    models['app_type'] = bv.build_vocab(sentences)
+    sentences = []
+
+    for key in files:
+        appcategory = files[key]['json']['details']['app_details']['app_category'][0]
+        app_type = files[key]['json']['details']['app_details']['app_type']
+
+        appcategory_vocab = models['app_category'].wv.vocab
+        app_type_vocab = models['app_type'].wv.vocab
+        files[key]['vec'] = []
+        files[key]['vec'].append(int(files[key]['json']['doc_type']))
+        files[key]['vec'].append(int(files[key]['json']['backend_id']))
+
+        files[key]['vec'].append(appcategory_vocab[appcategory].index/len(appcategory_vocab))
+        files[key]['vec'].append(app_type_vocab[app_type].index/len(app_type_vocab))
+        print(files[key]['vec'])
+
+    result = dict()
+    texts = list(files.keys())
+    for i in range(len(texts) - 1):
+        for j in range(i + 1, len(texts)):
+            file1 = texts[i]
+            file2 = texts[j]
+            reskey = tuple([file1, file2])
+            result[reskey] = dict()
+            simm = cs.Cosine(files[file1]['vec'], files[file2]['vec'])
+            result[reskey]['json'] = simm
+
+    return result
+
+
+
+def get_text_sim(long_des_dir, short_des_dir):
     '''
 
     :param long_des_dir: 长描述文件夹（不含子目录）
@@ -55,8 +119,12 @@ def process(long_des_dir, short_des_dir):
 def main():
     long_des_dir = 'resource\\testdata3_long\\'
     short_des_dir = 'resource\\testdata3_short\\'
+    json_dirpath = 'resource\\testdata_json\\'
 
-    process(long_des_dir, short_des_dir)
+
+    text_result = get_text_sim(long_des_dir, short_des_dir)
+    json_result = get_json_sim(json_dirpath)
+
 
 if __name__ == '__main__':
     main()
