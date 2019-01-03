401c2da7394aa0494cb9522145d355a3e1d44ec4
chengxiao19961022 <769478099@qq.com>
2019-01-03 18:57:53 +0800

    add ldalearn

--- learn/tfidf_learn.py
+++ learn/tfidf_learn.py
@@ -20,27 +20,35 @@ def tokenization(filename):
 
 def main():
     dirpath = '..\\resource\\learn_data'
+
     corpus = []
     for dirpath, dirnames, filenames in os.walk(dirpath):
         for file in filenames:
             fullpath = os.path.join(dirpath, file)
             corpus.append(tokenization(fullpath))
     dictionary = corpora.Dictionary(corpus)
-    doc_vectors = [dictionary.doc2bow(text) for text in corpus]
+    doc_vectors = [dictionary.doc2bow(text) for text in corpus]# e.g.[[(1,2),(2,3)],[(1,2),(2,3)]]前为下标，后为词频
+
     tfidf = models.TfidfModel(doc_vectors)
-    tfidfvectors = tfidf[doc_vectors]
+    tfidfvectors = tfidf[doc_vectors]# e.g.[[(1,0.332),(2,0.12)],[(1,0.22),(2,0.45)]]前为下标，后为tf-idf值
+
     # test = tfidfvectors[0]
     # index = similarities.MatrixSimilarity(tfidfvectors)
-    # sims = index[test] # 采用余弦相似度比较的方法，待比较向量是维度为dictionary的unique token的数量，将tfidf值填入对应下标，若无token则该下标处置0
+    # sims = index[test] # 采用余弦相似度比较的方法，待比较向量：维度为dictionary的unique token的数量，将tfidf值填入对应下标，若无token则该下标处置0
+
     lsi = models.LsiModel(tfidfvectors, id2word=dictionary, num_topics=2)
-    lsivectors = lsi[tfidfvectors]
+    lsivectors = lsi[tfidfvectors]# e.g.[[(0,0.5),(1,0.7)],[(0,0.5),(1,0.7)]]前为主题下标，后为属于该主题的概率
     index = similarities.MatrixSimilarity(lsivectors)
     test = lsivectors[0]
     sims = index[test]
-    test1 = [i[1] for i in test]
-    test2 = lsivectors[2]
-    test21 = [i[1] for i in test2]
-    sim2 = cs.Cosine(test1, test21)
+    # test1 = [i[1] for i in test]
+    # test2 = lsivectors[2]
+    # test21 = [i[1] for i in test2]
+    # sim2 = cs.Cosine(test1, test21)
+
+    lda = models.LdaModel(tfidfvectors, id2word=dictionary, num_topics=2)
+    ldavectprs = lda[tfidfvectors]# 同上
+    index = similarities.MatrixSimilarity(ldavectprs)
 
 
 
