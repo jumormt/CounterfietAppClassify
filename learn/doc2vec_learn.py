c7b2213da47870a2391080c025b209838dfaa5f1
chengxiao19961022 <769478099@qq.com>
2019-03-05 20:22:45 +0800

    [add] doc2vec learn

--- learn/doc2vec_learn.py
+++ learn/doc2vec_learn.py
@@ -6,16 +6,12 @@ from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(common_texts)]
 model = Doc2Vec(documents, vector_size=5, window=2, min_count=1, workers=4)
 
-# print(model.docvecs.most_similar('0'))
-# # 进行相关性比较
-# print(model.docvecs.similarity())
-# # 输出标签为‘10’句子的向量
-# print(model.docvecs['10'])
-# # 也可以推断一个句向量(未出现在语料中)
-# words = u"여기 나오는 팀 다 가슴"
-# print(model.infer_vector(words.split()))
-# # 也可以输出词向量
-# print(model[u'가슴'])
+# todo:增量学习
+# model.build_vocab(documents[3:],update=True)
+#
+# model.train(documents[3:], total_examples=model.corpus_count, epochs=model.iter)
 model.infer_vector(['human'])
+# print(model.docvecs.distance())
+print(model.corpus_count)
 
 print("end")
